ac29114acc4408f6fd01590837f232bb637fb964
Leedogeon <jszs98@naver.com>
2025-07-11 17:25:23 +0900

    이동, 인풋관련 베이스 코드 작성
    이동, 인풋관련 베이스 코드 작성

--- Assets/Script/PlayerInput.cs
+++ Assets/Script/PlayerInput.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // 다른 스크립트에서 입력값을 받을수는 있지만, 수정은 불가능하게 캡슐화
+    // 플레이어의 인풋에관한 모든 처리는 여기서 작성
+    // 자세한 코드는 해당하는 스크립트에서 작성
     public Vector2 MoveInput { get; private set; }
     public bool AttackInput { get; private set; }
 
     // Update is called once per frame
     void Update()
     {
+        // 대각선 이동거리 문제로 인한 normalized는 인풋에서 처리
         MoveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        // 공격은 마우스 좌클릭으로 설정
         AttackInput = Input.GetMouseButtonDown(0);
     }
 }
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // 이동관련 스크립트
+    // 인풋자체는 PlayerInput에서 받아오고, 나머지 이동관련 스크립트는 여기서 작성
     PlayerInput Input;
     Rigidbody Rigid;
+    // 유니티 자체에서 컨트롤하며 조정하기위해 [SerializeField] 사용
     [SerializeField] public float speed = 5f;
 
     private void Awake()
@@ -16,7 +20,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 dir = new Vector3(Input.MoveInput.x,0,Input.MoveInput.y);
-        Rigid.MovePosition(transform.position + dir * speed * Time.fixedDeltaTime);
+        // 이동값, normalized는 input에서 미리 처리
+        Vector3 dir = new Vector3(Input.MoveInput.x, 0, Input.MoveInput.y);
+        // 자연스럽게 하기위해 velocity 사용
+        Rigid.velocity = dir * speed;
+
     }
 }
