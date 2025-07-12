0c9d0e7dc0fcf13e760711d99c42b649a3e6a032
Leedogeon <jszs98@naver.com>
2025-07-12 12:59:38 +0900

    Input 함수가 있어서 input으로 변경
    Input 함수가 있어서 input으로 변경
    회전 추가

--- Assets/Script/PlayerScript/PlayerMovement.cs
+++ Assets/Script/PlayerScript/PlayerMovement.cs
@@ -7,21 +7,22 @@ public class PlayerMovement : MonoBehaviour
 {
     // 이동관련 스크립트
     // 인풋자체는 PlayerInput에서 받아오고, 나머지 이동관련 스크립트는 여기서 작성
-    PlayerInput Input;
+    PlayerInput input;
     Rigidbody Rigid;
     // 유니티 자체에서 컨트롤하며 조정하기위해 [SerializeField] 사용
     [SerializeField] public float speed = 5f;
+    float rotateSpeed = 10f;
 
     private void Awake()
     {
-        Input = GetComponent<PlayerInput>();
+        input = GetComponent<PlayerInput>();
         Rigid = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
         // 이동값, normalized는 input에서 미리 처리
-        Vector3 dir = new Vector3(Input.MoveInput.x, 0, Input.MoveInput.y);
+        Vector3 dir = new Vector3(input.MoveInput.x, 0, input.MoveInput.y);
         // 자연스럽게 하기위해 velocity 사용
         //Rigid.velocity = dir * speed;
         // 중력의 영향을 정상적으로 받기위해 y값을 따로 설정
@@ -29,6 +30,12 @@ public class PlayerMovement : MonoBehaviour
         Vector3 newVelocity = dir * speed;
         newVelocity.y = Rigid.velocity.y;
         Rigid.velocity = newVelocity;
+
+        // 캐릭터의 forward를 이용해서 회전시킨다
+        // Slerp를 이용하여 자연스럽게 회전하게 하고
+        // 회전에 y값은 영향받지 않게 할려고 newVelocity가 아닌 dir을 이용
+        // rotateSpeed는 적당히 자연스러운 느낌을 받게 커스텀 조정
+        transform.forward = Vector3.Slerp(transform.forward, dir, Time.deltaTime * rotateSpeed);
 
     }
 }
