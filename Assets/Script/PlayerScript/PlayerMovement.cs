71a632551693f94397be896ee46d8ef200533706
Leedogeon <jszs98@naver.com>
2025-08-01 19:23:12 +0900

    걷기관련 추가
    걷기관련 추가

--- Assets/Script/PlayerScript/PlayerMovement.cs
+++ Assets/Script/PlayerScript/PlayerMovement.cs
@@ -9,14 +9,17 @@ public class PlayerMovement : MonoBehaviour
     // 인풋자체는 PlayerInput에서 받아오고, 나머지 이동관련 스크립트는 여기서 작성
     PlayerInput input;
     Rigidbody Rigid;
+    PlayerAnimationController animCon;
     // 유니티 자체에서 컨트롤하며 조정하기위해 [SerializeField] 사용
     [SerializeField] public float speed = 5f;
+    float walkSpeed = .5f;
     float rotateSpeed = 10f;
 
     private void Awake()
     {
         input = GetComponent<PlayerInput>();
         Rigid = GetComponent<Rigidbody>();
+        animCon = GetComponent<PlayerAnimationController>();
     }
 
     private void FixedUpdate()
@@ -27,17 +30,19 @@ public class PlayerMovement : MonoBehaviour
         //Rigid.velocity = dir * speed;
         // 중력의 영향을 정상적으로 받기위해 y값을 따로 설정
         // x,z값의 경우 speed의 영향을 받아야하니 먼저 곱해주고 y값은 따로 설정
-        Vector3 newVelocity = dir * speed;
+        Vector3 newVelocity = dir * speed * (input.WalkInput ? walkSpeed : 1 );
         if (Rigid.velocity.y > 0.5f) newVelocity.y = 0;
         else newVelocity.y = Rigid.velocity.y;
         Rigid.velocity = newVelocity;
+
+        animCon.MoveDirection(input.MoveInput,input.WalkInput);
 
         // 캐릭터의 forward를 이용해서 회전시킨다
         // Slerp를 이용하여 자연스럽게 회전하게 하고
         // 회전에 y값은 영향받지 않게 할려고 newVelocity가 아닌 dir을 이용
         // rotateSpeed는 적당히 자연스러운 느낌을 받게 커스텀 조정
         // 임시로 공격키가 입력중일땐 방향전환은 하지않도록 설정 - 이후 공격 모션도중 안하는걸로 수정
-        if(!input.AttackInput)
+        if (!input.AttackInput)
         transform.forward = Vector3.Slerp(transform.forward, dir, Time.deltaTime * rotateSpeed);
 
     }
