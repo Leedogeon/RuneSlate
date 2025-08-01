113d795e2c00539341ed5797405c545622c620a6
Leedogeon <jszs98@naver.com>
2025-08-01 19:22:23 +0900

    걷기키 추가
    걷기인풋추가

--- Assets/Script/PlayerScript/PlayerInput.cs
+++ Assets/Script/PlayerScript/PlayerInput.cs
@@ -8,7 +8,10 @@ public class PlayerInput : MonoBehaviour
     // 자세한 코드는 해당하는 스크립트에서 작성
     public Vector2 MoveInput { get; private set; }
     public bool AttackInput { get; private set; }
+    public bool WalkInput { get; private set; }
 
+    MapManager mapManager;
+    PlayerAnimationController animCon;
     #region 스킬관련
     static int SkillNum = 4;
 
@@ -21,22 +24,29 @@ public class PlayerInput : MonoBehaviour
 
     #endregion
     // Update is called once per frame
+
+    private void Awake()
+    {
+        mapManager = FindObjectOfType<MapManager>();
+        animCon = GetComponent<PlayerAnimationController>();
+    }
     void Update()
     {
+        if (!mapManager.CanInteraction) return;
         // 대각선 이동거리 문제로 인한 normalized는 인풋에서 처리
         //MoveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         // 상하좌우 입력키값을 변경가능하게 만들기위해 변경
         MoveInput = MoveInput_CanChange();
-        
         // 공격은 마우스 좌클릭으로 설정
         // 공격도중에 회전을 막기위해 임시로 GetMoustButton으로 변경, 이후 공격모션 도중으로 변경하는 등으로 변경
         //AttackInput = Input.GetMouseButton(0);
         // 입력값변화를 위해 KeyManger를 만들어 활용
-        AttackInput = Input.GetKeyDown(KeySetting.keys[KeyAction.ATTACK]);
+        AttackInput = Input.GetKey(KeySetting.keys[KeyAction.ATTACK]);
         SkillInput[0] = Input.GetKeyDown(KeySetting.keys[KeyAction.SKILL1]);
         SkillInput[1] = Input.GetKeyDown(KeySetting.keys[KeyAction.SKILL2]);
         SkillInput[2] = Input.GetKeyDown(KeySetting.keys[KeyAction.SKILL3]);
         SkillInput[3] = Input.GetKeyDown(KeySetting.keys[KeyAction.SKILL4]);
+        WalkInput = Input.GetKey(KeySetting.keys[KeyAction.WALK]);
 
     }
 
