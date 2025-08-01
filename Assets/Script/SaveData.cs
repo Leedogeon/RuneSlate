a75d9213184d494c8ea2b802197e59f1711e5c64
Leedogeon <jszs98@naver.com>
2025-08-01 21:26:14 +0900

    저장용 JSON파일
    저장용 JSON파일

---
+++ Assets/Script/SaveData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] // 이게있어야 JsonUtility가 직렬화 가능
+public class SaveData
+{
+    public float playerX;
+    public float playerY;
+    public float playerZ;
+
+    public int hp;
+    public int level;
+
+}
