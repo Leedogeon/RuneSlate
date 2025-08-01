b1714c250e465044127590c8701d5db3a5cbeabb
Leedogeon <jszs98@naver.com>
2025-08-01 19:19:47 +0900

    캔버스 활성화시 조작 금지
    옵션창 추가, 캔버스 활성화 함수

--- Assets/Script/MapManager.cs
+++ Assets/Script/MapManager.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     public Transform SpawnPoint;
-
     [SerializeField] private GameObject prologue;
+    public bool CanInteraction = false;
+    [SerializeField] private GameObject OptionPanel;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // mapManager의 invisivleObj을 이용하여 처리
+            invisivleObj(OptionPanel);
+            // 속도증감, 0 -> 정지
+            Time.timeScale = CanInteraction ? 1 : 0;
+        }
+    }
+
+    // 다른 Obj들에게도 적용하기위해 변경 - 08.01
     public void invisivleObj(GameObject obj)
     {
-        obj.SetActive(false);
+        if (obj == null) return;
+        // 현재 obj의 활성화 상태를 이용
+        bool isAct = obj.activeSelf;
+        obj.SetActive(!isAct);
+        CanInteraction = isAct;
     }
+
 
 }
