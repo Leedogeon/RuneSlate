2579f996bfa3051a1e0ff0620f95c96763ed6e8a
Leedogeon <jszs98@naver.com>
2025-08-19 20:23:07 +0900

    플레이어 기본스텟 JSON
    플레이어 기본스텟 JSON

---
+++ Assets/Script/PlayerScript/PlayerDefaultState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable] // 이게있어야 JsonUtility가 직렬화 가능
+public class PlayerDefaultState
+{
+    public float BaseHp = 100;
+
+}
