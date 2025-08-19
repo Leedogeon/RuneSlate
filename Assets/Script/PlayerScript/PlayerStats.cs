90a83094f98b346adbfab73ae7b67d93a78392cd
Leedogeon <jszs98@naver.com>
2025-08-19 20:24:54 +0900

    저장 JSON
    저장 JSON

---
+++ Assets/Script/PlayerScript/PlayerStats.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float maxHp;
+    public float curHP;
+
+    [SerializeField]PlayerHealth healthUI;
+
+    private void Awake()
+    {
+        LoadPlayerDefaultStats();
+        healthUI = FindObjectOfType<PlayerHealth>(true);
+
+    }
+    public void LoadPlayerDefaultStats()
+    {
+        PlayerDefaultState defaultState = new PlayerDefaultState();
+        maxHp = defaultState.BaseHp;
+        curHP = 10;
+    }
+    
+}
