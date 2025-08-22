9eaf2d33084a4a6b327b308840d3b328e827f4f5
Leedogeon <jszs98@naver.com>
2025-08-22 20:48:02 +0900

    세이브 관련

---
+++ Assets/Script/PlayerDataControll.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerDataControll
+{
+    public static Vector3 PlayerStartPosFromLoad;
+    public static int SaveLoadIndex = -1;
+    public static int PlayTime = 0;
+    public static int Lv = 1;
+}
