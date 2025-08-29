5a1420554d1d547201e146707fb8223736d73c0d
Leedogeon <jszs98@naver.com>
2025-08-29 16:21:48 +0900

    ! 발생 테스트

--- Assets/Script/PlayerScript/Player.cs
+++ Assets/Script/PlayerScript/Player.cs
@@ -6,11 +6,12 @@ public class Player : MonoBehaviour
 {
     // 다른 스크립트에서 입력값을 받을수는 있지만, 수정은 불가능하게 캡슐화
     // 코드를 나눠서 제작중
-    [SerializeField] PlayerInput Input;
+    [SerializeField] PlayerInput input;
     [SerializeField] PlayerMovement Movement;
+    [SerializeField] GameObject questionMark;
     private void Awake()
     {
-        Input = GetComponentInChildren<PlayerInput>();
+        input = GetComponentInChildren<PlayerInput>();
         Movement = GetComponentInChildren<PlayerMovement>();
     }
 
@@ -19,10 +20,21 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
-        // 공격 인풋 테스트중
-        if(Input.AttackInput)
-        {
-        }
 
+    }
+
+    public void SpawnQuestionMark()
+    {
+        Vector3 qPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 2.3f, gameObject.transform.position.z);
+        GameObject Mark = Instantiate(questionMark, qPos, Quaternion.identity);
+        StartCoroutine(StopPlayer());
+        Destroy(Mark,.3f);
+    }
+    public IEnumerator StopPlayer()
+    {
+        Time.timeScale = 0;
+        // 실제시간을 체크
+        yield return new WaitForSecondsRealtime(1f);
+        Time.timeScale = 1;
     }
 }
