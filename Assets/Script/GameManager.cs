19b97a6532da9e697a2d77986047fed6c35c98e3
Leedogeon <jszs98@naver.com>
2025-09-13 01:40:31 +0900

    보스소환 추가중

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -10,6 +10,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] TalkUIScript TalkScript;
     [SerializeField] TextScript TxtScript;
     [SerializeField] QuestUIText questText;
+    [SerializeField] MapManager mapManager;
+    [SerializeField] GameObject ElitePref;
+    [SerializeField] GameObject SpawnEffect;
     private void OnEnable()
     {
         Enemy.OnEnemyDeath += QuestUIText.UpdateQuestProgress;
@@ -21,6 +24,7 @@ public class GameManager : MonoBehaviour
     }
     private void Start()
     {
+        mapManager = FindObjectOfType<MapManager>();
         TextUI();
     }
     public void TextUI()
@@ -48,4 +52,51 @@ public class GameManager : MonoBehaviour
         questText.ShowQuest(PlayerDataControll.CurQuestId++);
     }
 
+    // 임시처리
+    public void QuestOpen(int index)
+    {
+        Debug.Log("CurQuestId = " + PlayerDataControll.CurQuestId);
+        questText.gameObject.SetActive(true);
+        questText.ShowQuest(index);
+    }
+
+    public void SpawnElite()
+    {
+        StartCoroutine(SpawnBoss());
+    }
+
+
+    IEnumerator SpawnBoss()
+    {
+        yield return new WaitForSeconds(1f);
+        questText.gameObject.SetActive(false);
+        // Boss 소환
+        GameObject Boss = Instantiate(ElitePref, mapManager.ElitePos, ElitePref.transform.rotation);
+
+        PlayerDataControll.CanControll = false;
+        // 다시 1초 대기
+        yield return new WaitForSeconds(1f);
+        GameObject Effect = Instantiate(SpawnEffect, mapManager.ElitePos, SpawnEffect.transform.rotation);
+        yield return new WaitForSeconds(1f);
+        // 2초 동안 scale Lerp
+        Vector3 originalScale = Boss.transform.localScale;
+        Vector3 targetScale = originalScale * 1.3f;
+        float duration = 2f;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            Boss.transform.localScale = Vector3.Lerp(originalScale, targetScale, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(Effect);
+        QuestOpen(5);
+
+        // 최종값 보정
+        Boss.transform.localScale = targetScale;
+        PlayerDataControll.CanControll = true;
+        Boss.GetComponent<Enemy_TutorialBoss>().CanAttack = true;
+    }
 }
